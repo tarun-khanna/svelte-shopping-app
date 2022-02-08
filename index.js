ad750d5341385cb60bdb810857ef3ca4b58eb155
tarun khanna <tarun.khanna@razorpay.com>
2022-02-08 14:54:09 +0530

    razorpay signature verified for payment at server

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
 // middlewares
 app.use(express.json({ extended: false }));
 
--- routes/payment.js
+++ routes/payment.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const Razorpay = require('razorpay');
 const nanoid = require('nanoid').nanoid;
+const crypto = require('crypto');
 
 const router = express.Router();
 
@@ -29,7 +30,26 @@ router.post('/orders', async (req, res) => {
 
     res.json(order);
   } catch (error) {
-    console.log('🚀 ~ file: payment.js ~ line 32 ~ router.post ~ error', error);
+    res.status(500).send(error);
+  }
+});
+
+router.post('/verify', async (req, res) => {
+  try {
+    const { orderCreationId, razorpayPaymentId, razorpayOrderId, razorpaySignature } = req.body;
+
+    const shasum = crypto.createHmac('sha256', process.env.RAZORPAY_SECRET);
+    shasum.update(`${orderCreationId}|${razorpayPaymentId}`);
+    const digest = shasum.digest('hex');
+
+    if (digest !== razorpaySignature) return res.status(400).json({ msg: 'Transaction not legit!' });
+
+    res.json({
+      msg: 'success',
+      orderId: razorpayOrderId,
+      paymentId: razorpayPaymentId,
+    });
+  } catch (error) {
     res.status(500).send(error);
   }
 });
